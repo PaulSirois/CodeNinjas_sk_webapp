7b3c8612fc74eac5c56e95196a5902af76bb2c96
FaizanRaji <tritops11@gmail.com>
2023-12-24 10:52:47 -0600

    changed a few things
    - repositioned the card/rectangle
    - put the user icon and name into the card
    - added ninjas icon and text
    - changed the user icon
    - added a logout text to the bottom of the card

--- frontend/code_ninjas_webapp_frontend/cn_webapp_frontend/lib/main.dart
+++ frontend/code_ninjas_webapp_frontend/cn_webapp_frontend/lib/main.dart
@@ -15,25 +15,77 @@ class MyApp extends StatelessWidget {
         backgroundColor: Color.fromRGBO(24, 128, 179, 1),
         body: Center(
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              SizedBox(
-                height: 900,
-                width: 250,
-                child: Card(
-                  color: Color.fromRGBO(255, 255, 255, 1),
-                  child: Padding(
-                    padding: EdgeInsets.all(20),
+              Padding(
+                padding: EdgeInsets.only(left: 20.0),
+                child: SizedBox(
+                  height: 950,
+                  width: 250,
+                  child: Card(
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Align(
+                                alignment: Alignment.topCenter,
+                                child: Icon(
+                                  Icons.account_circle,
+                                  size: 60,
+                                ),
+                              ),
+                              SizedBox(width: 10),
+                              Align(
+                                alignment: Alignment.topCenter,
+                                child: Text(
+                                  'Admin',
+                                  style: TextStyle(fontSize: 30),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 20),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Align(
+                                alignment: Alignment.topCenter,
+                                child: Icon(
+                                  Icons.people,
+                                  size: 60,
+                                ),
+                              ),
+                              SizedBox(width: 10),
+                              Align(
+                                alignment: Alignment.topCenter,
+                                child: Text(
+                                  'Ninjas',
+                                  style: TextStyle(fontSize: 30),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Text(
+                              'Logout',
+                              style: TextStyle(
+                                  fontSize: 30,
+                                  color: Colors.red,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              Icon(
-                Icons.person,
-                size: 50,
-              ),
-              Text(
-                'Admin',
-                style: TextStyle(fontSize: 30),
               ),
             ],
           ),
