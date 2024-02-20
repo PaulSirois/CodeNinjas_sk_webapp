4f73b6298ff4cc2e76d540c457597cd01207841e
Paul Sirois <pas379@usask.ca>
2024-02-20 16:44:33 -0600

    not done: made changes to ninjas table design

--- db/setup/setup.sql
+++ db/setup/setup.sql
@@ -19,10 +19,20 @@ CREATE TABLE IF NOT EXISTS new_senseis (
     name varchar(200) NOT NULL UNIQUE,
     role ENUM('sensei', 'front-desk', 'admin'),
     PRIMARY KEY (name));
-CREATE TABLE IF NOT EXISTS ninjas
-        (name varchar(100),
-        belt enum('white', 'yellow', 'orange', 'green', 'blue', 'purple', 'brown', 'red', 'black'),
-        current_activity varchar(10));
+CREATE TABLE IF NOT EXISTS ninjas (
+    name varchar(100),
+    belt enum('white', 'yellow', 'orange', 'green', 'blue', 'purple', 'brown', 'red', 'black'),
+    level tinyint unsigned, constraint level_out_of_range check (level <= 0 and level >= 13),
+    activity tinyint unsigned, constraint activity_out_of_range check (activity <= 0 and activity >= 6),
+    laptop_number tinyint,
+    coins smallint unsigned);
+CREATE TABLE IF NOT EXISTS levels (
+    id int auto_increment,
+    name varchar(50) not null,
+    belt enum('white', 'yellow', 'orange', 'green', 'blue', 'purple', 'brown', 'red', 'black'),
+    level tinyint unsigned, constraint level_out_of_range check (level <= 0 and level >= 13),
+    activity tinyint unsigned, constraint activity_out_of_range check (activity <= 0 and activity >= 6)
+);
 INSERT INTO users (id_bin, name, password, role) VALUES (UNHEX(REPLACE(UUID(),'-','')), 'SUPERUSER', 'ADMINPASSWORD', 'admin')
 ON DUPLICATE KEY UPDATE id_bin=UNHEX(REPLACE(UUID(),'-',''));
 # CREATE USER 'postit-user'@'localhost' IDENTIFIED BY 'postIT-super-secret-password';
